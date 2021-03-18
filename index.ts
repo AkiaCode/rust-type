2f79fe788ae6637c8ee68ab07ef5415948631e78
Akiacode <catry.me@gmail.com>
2021-03-18 14:43:35 +0900

    Add isErr, isOk

--- index.ts
+++ index.ts
@@ -2,13 +2,29 @@ export type OptineType<T> = T | undefined;
 export type ResultType<T, E> = T | E;
 
 export class Optine<T> {
+    some: T;
     public Some(some: T): T { return some; }
     public None = undefined;
+    public isSome(v: T): boolean {
+        return String(v) === String(this.some)
+    }
 }
 
 export class Result<T, E> {
     ok: T;
     err: E;
-    public Ok(ok: T): T { return ok };
-    public Err(err: E): E { return err };
+    public Ok(ok: T): T {
+        this.ok = ok;
+        return ok
+    };
+    public Err(err: E): E {
+        this.err = err;
+        return err
+    };
+    public isOk(v: T): boolean {
+        return v === this.ok
+    }
+    public isErr(v: E): boolean {
+        return v === this.err
+    }
 }
